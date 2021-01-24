e8b503d78fdeedc99f510d2b924c666e182d0f91
Patrick Dunham <patrick.dunham.113@gmail.com>
2021-01-24 14:05:06 -0500

    Format recipe

--- prototype/recipe/power-decoupler.lua
+++ prototype/recipe/power-decoupler.lua
@@ -1,17 +1,13 @@
-data:extend(
-{
-    {
+data:extend({{
     type = "recipe",
     name = "power-decoupler",
     energy_required = 10,
     enabled = false,
-    ingredients =
-    {
+    ingredients = {
         {"accumulator", 1},
         {"iron-plate", 5},
         {"processing-unit", 2},
         {"battery", 5}
     },
     result = "power-decoupler"
-    }
-})
+}})
