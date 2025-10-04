21ea266d6fb8b71c295c9138540c41c4fc0bbf81
PDunham113 <patrick.dunham.113@gmail.com>
2025-10-03 21:35:47 -0700

    fix crash on startup when Space Exploration is installed
    Not bumping release number since I already screwed that up

--- prototype/recipe/power-decoupler.lua
+++ prototype/recipe/power-decoupler.lua
@@ -4,10 +4,10 @@ data:extend({{
     energy_required = 10,
     enabled = false,
     ingredients = {
-        {"accumulator", 1},
-        {"iron-plate", 5},
-        {"advanced-circuit", 2},
-        {"battery", 5}
+        {type = "item", name = "accumulator", amount = 1},
+        {type = "item", name = "iron-plate", amount = 5},
+        {type = "item", name = "advanced-circuit", amount = 2},
+        {type = "item", name = "battery", amount = 5}
     },
     result = "power-decoupler"
 }})
